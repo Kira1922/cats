ed691c09ce95a006007dcb2b802025ae4d6c39e7
Kira1922 <kirap996@gmail.com>
2025-07-25 00:34:50 +0300

    добавлено получение изображений по тегу

--- cats2.py
+++ cats2.py
@@ -17,12 +17,14 @@ def load_image(url):
 
 
 def open_new_window():
-    img = load_image(url)
+    tag = tag_entry.get()
+    url_with_tag = f'https://cataas.com/cat/{tag}' if tag else 'https://cataas.com/cat'
+    img = load_image(url_with_tag)
+
     if img:
         new_window = Toplevel()
         new_window.title("Картинка с котиком")
         new_window.geometry("600x480")
-
         label = Label(new_window, image=img)
         label.image = img
         label.pack()
@@ -39,8 +41,13 @@ window.geometry("600x520")
 label = Label()
 label.pack()
 
-#update_button = Button(text="Обновить", command=set_image)
-#update_button.pack()
+tag_entry = Entry()
+tag_entry.pack()
+
+
+load_button = Button(text="Загрузить по тегу", command=open_new_window)
+load_button.pack()
+
 
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
